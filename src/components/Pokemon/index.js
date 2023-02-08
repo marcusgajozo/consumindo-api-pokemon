7a15dcc9200cee54846b4852b026addc446416a0
Marcus Gajozo <marcusgajozo@gmail.com>
2023-02-08 18:10:05 -0400

    imagens de pokemons adicionados ao componente Pokemon

--- src/components/Pokemon/index.js
+++ src/components/Pokemon/index.js
@@ -1,6 +1,28 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import { Card } from "./style";
 
 export default function Pokemon({ data }) {
-  return <Card>{data.name}</Card>;
+  const [Pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    axios.get(data.url).then((response) => {
+      console.log(response.data);
+      setPokemon(response.data);
+    });
+  }, []);
+
+  return (
+    <Card>
+      <span>{Pokemon.name}</span>
+      <span>
+        <img
+          src={Pokemon.sprites.other.dream_world.front_default}
+          alt={`imagem do pokemon ${Pokemon.name}`}
+        />
+      </span>
+    </Card>
+  );
 }
